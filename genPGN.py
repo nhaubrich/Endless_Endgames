f46976e472b69bafcb2b5db8f39cbc4e41037da3
Nick Haubrich <nicholasjhaubrich@gmail.com>
2023-03-19 17:34:03 -0400

    Working prototype

--- genPGN.py
+++ genPGN.py
@@ -62,56 +62,102 @@ def throwPiece(board,type_of_piece=None):
         position = random.randint(0,63)
     return board[0:position] + type_of_piece + board[position+1:64]
 
-#for x in range(5):
-while True:
-    validPosition=False
-    while validPosition!=True:
-        print("try")
-        pgn = genPGN()
-        validPosition=chess.Board(pgn.replace("_"," ")).is_valid()
 
-    #print(pgn)
-    #output = subprocess.check_output(['curl','http://tablebase.lichess.ovh/standard?fen=k7/p7/8/8/8/8/8/K7_w_-_-_0_1'])
-    #gameResult=os.popen('curl http://tablebase.lichess.ovh/standard?fen=k7/p7/8/8/8/8/8/K7_w_-_-_0_1').read()
-    #gameResultTxt=os.popen('curl http://tablebase.lichess.ovh/standard?fen='+pgn).read()
-    print("curl http://tablebase.lichess.ovh/standard?fen="+pgn)
-    #gameResultTxtFull=subprocess.Popen(["curl","http://tablebase.lichess.ovh/standard?fen="+pgn],stdout=subprocess.PIPE,stderr=subprocess.DEVNULL)
+def query_tablebase(pgn):
+    #print("curl http://tablebase.lichess.ovh/standard?fen="+pgn)
+    
     gameResultTxtFull=subprocess.Popen(["curl","-s","-w","'%{stderr}%{http_code}%{stdout}'","http://tablebase.lichess.ovh/standard?fen="+pgn],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     gameResultTxt=gameResultTxtFull.stdout.read()
     gameResultErr=gameResultTxtFull.stderr.read()
     if gameResultErr==b"429": 
-        print(gameResultErr,gameReultTxt)
+        print(gameResultErr,gameResultTxt)
         time.sleep(60)
+        gameResult=None
     elif gameResultErr!=b"200":
-        print(gameResultErr,gameReultTxt)
+        print(gameResultErr,gameResultTxt)
+        gameResult=None
     else: 
     #try:
         gameResult = json.loads(gameResultTxt[:-2]) #where are these slashes coming from?
 
+    return gameResult
+
+#for x in range(5):
+while True:
+    validPosition=False
+    while validPosition!=True:
+        #print("try")
+        pgn = genPGN()
+        #pgn = "3q4/8/2b5/1K5k/P7/7n/8/8_w_-_-_0_1"
+        #pgn = "3kN3/8/4B3/8/K7/4B3/2p5/8_b_-_-_0_1"
+        boardState=chess.Board(pgn.replace("_"," "))
+        validPosition=boardState.is_valid()
+
+    gameResult = query_tablebase(pgn)
+    
+    if gameResult!=None:
         result = gameResult['category']
         print(result+"\t"+str(gameResult['dtm']),pgn)
-        pdb.set_trace()
-        if gameResult['dtm']>=50:
-            print(pgn)
-            with open("games.txt","a") as f:
-                player = pgn.split("_")[1]
-                if player=="w":
-                    player = "White"
-                else:
-                    player = "Black"
-                f.write(result+"\t"+str(gameResult['dtm']) + "\t" + pgn+"\n")
-                #pdb.set_trace()
-                text = "{} mates in {}".format(player,gameResult['dtm']) 
-                pngCommand = ["python3","fen-to-img/main.py"]+pgn.split("_")+["-o",player+"_"+str(gameResult['dtm'])+"_"+pgn.replace("/","").replace("-","")]+["-d","pngs/"]
-                pngCommand[4]="KQ"
-                print(" ".join(pngCommand))
-                subprocess.Popen(pngCommand)
+        
 
-        #if result in ['cursed-win','blessed-loss']:
-            #print(gameResult['dtm'],result,"for white!")
-    #except:
-        #print(gameResultTxt)
-    #    pass
+        if result=="loss":
+            #print("flip")
+            #make best move, result="win"
+            boardState.push(chess.Move.from_uci(gameResult['moves'][0]['uci']))
+            boardState.fullmove_number-=1
+            pgn = boardState.fen().replace(" ","_")
+            gameResult = query_tablebase(pgn)
+            result="win"
+
+
+
+        if result=="win":
+
+            if gameResult['dtm']==None:
+                #crawl through dtz until dtm is known
+                crawlGameResult = gameResult
+                n=0
+                while crawlGameResult['dtm']==None and not crawlGameResult['checkmate']: #break if checkmate or dtm!=none
+                    time.sleep(0.5)
+                    boardState.push(chess.Move.from_uci(crawlGameResult['moves'][0]['uci']))
+                    crawlPGN = boardState.fen().replace(" ","_")
+
+                    crawlGameResult = query_tablebase(crawlPGN)
+                    #print(crawlGameResult)
+                    n+=1
+        
+                #pdb.set_trace()
+                if crawlGameResult['dtm']==None:
+                    dtm = boardState.fullmove_number*2
+                else:
+
+                    dtm = boardState.fullmove_number*2+abs(crawlGameResult['dtm'])
+                #print("Crawled {}, dtm:{}".format(n,dtm) )
+                gameResult["dtm"] = dtm
+
+            if gameResult['dtm']!=None and gameResult['dtm']>=50:
+                print("MOVES {}".format(gameResult['dtm']),pgn)
+                with open("games.txt","a") as f:
+                    player = pgn.split("_")[1]
+                    if player=="w":
+                        player = "White"
+                    else:
+                        player = "Black"
+                    f.write(result+"\t"+str(gameResult['dtm']) + "\t" + pgn+"\n")
+                    text = "{} mates in {}".format(player,gameResult['dtm']) 
+                    pngCommand = ["python3","fen-to-img/main.py"]+pgn.split("_")+["-o",player+"_"+str(gameResult['dtm'])+"_"+pgn.replace("/","").replace("-","")]+["-d","pngs/"]
+                    pngCommand[4]="KQ"
+                    print(" ".join(pngCommand))
+                    subprocess.Popen(pngCommand)
+
+
+
+
+            #if result in ['cursed-win','blessed-loss']:
+                #print(gameResult['dtm'],result,"for white!")
+        #except:
+            #print(gameResultTxt)
+        #    pass
     time.sleep(1)
 
 
