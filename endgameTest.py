5fd7d7023eb75aa2ae14a12d738ab09b1b4ed0c6
Nick Haubrich <nicholasjhaubrich@gmail.com>
2024-10-18 17:10:48 -0400

    Add command line options

--- endgameTest.py
+++ endgameTest.py
@@ -1,5 +1,6 @@
 #generate random king+pawn v king endgames, then link to lichess study and see if win or loss
 from genPGN import *
+import sys
 
 def GetRank(piece,board):
     if piece in board:
@@ -46,41 +47,31 @@ def KingAndPawnVKingAndPawn():
     
     player = "wb"[random.randint(0,1)]
 
-
     return convertToPGN(board,player)
 
-def KRPvKR():
+def ThrowPieces(pieces, player="r"):
     board = "x"*64
-    board = throwPiece(board,"K")
-    board = throwPiece(board,"k")
-    
-    board = throwPiece(board,"R")
-    board = throwPiece(board,"r")
-    
-    if random.randint(0,1):
-        board = throwPiece(board,"P")
-    else:
-        board = throwPiece(board,"p")
-    
-    player = "wb"[random.randint(0,1)]
-
+    for piece in pieces:
+        board = throwPiece(board,piece)
+    if player=="r":
+        player = "wb"[random.randint(0,1)]
 
     return convertToPGN(board,player)
-
 
 if __name__=="__main__":
-    #engine = chess.engine.SimpleEngine.popen_uci("/usr/games/stockfish")
-    #limit=chess.engine.Limit(time=20)
-
-    
-    for i in range(20):
-        #game = KingAndPawnVKingAndPawn()
+    pieces = sys.argv[1]
+    player = sys.argv[2]
+    iterations = sys.argv[3]
+    assert set(pieces)<=set("KkQqRrBbNnPp"), "Invalid piece set {} . Piece options are subsets of KkQqRrBbNnPp, including duplicates.".format(pieces)
+    assert player in ["w","b","r"], "Invalid player {}. Options are (w)hite, (b)lack, (r)andom.".format(player)
+    assert int(iterations)>=1, "Invalid iterations {}. Must be positive integer.".format(iterations)
+    for i in range(int(iterations)):
+        
         validPosition=False
         while not validPosition:
-            game=KRPvKR()
+            game=ThrowPieces(pieces,player)
             boardState=chess.Board(game.replace("_"," "))
             validPosition=boardState.is_valid()
-
 
         url = "http://lichess.org/analysis/"+game
         print(url)
